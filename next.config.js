4bc6b110b02c0f8d3cc607e14b35defa4b2b8550
HSSAINI Saad <hssainisaad@gmail.com>
2024-08-09 11:34:50 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/platform/:path*',
+        source: '/platform/:path*',
         destination:
             process.env.NODE_ENV === 'development'
-                ? 'http://127.0.0.1:5328/api/platform/:path*'
+                ? 'http://127.0.0.1:5328/:path*'
                 : '/api/',
       },
     ]
